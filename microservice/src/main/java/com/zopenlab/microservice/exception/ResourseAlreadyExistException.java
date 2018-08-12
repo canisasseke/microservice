d2477f4548dd658d5c838615367036a9f40ebf73
kaskinas <kaskinas@yahoo.fr>
2018-08-12 09:28:38 +0000

    premier commit 1

--- microservice/src/main/java/com/zopenlab/microservice/exception/ResourseAlreadyExistException.java
+++ microservice/src/main/java/com/zopenlab/microservice/exception/ResourseAlreadyExistException.java
@@ -2,6 +2,11 @@ package com.zopenlab.microservice.exception;
 
 public class ResourseAlreadyExistException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public ResourseAlreadyExistException(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
